568536cdccb3a39ac306f9315cd0fea3eb02a8ed
crvicek <crvicek@yahoo.com>
2019-01-09 10:47:00 +0100

    Set up js files and declare submitComment

---
+++ detail-page.js
@@ -0,0 +1,14 @@
+/* A user can leave their name and write a comment, when they click submit, their name and comment will be displayed on the page
+
+Create an input field for the name, one for the comment and an submit-button
+Add an event (name and comment) listener/handler to the button
+Gather data of the input fields
+Create some elements (to display the data)
+Adjust the elements so they display the data
+Display the adjusted elements on your page
+Empty the input fields so the next user can type some new text.
+*/
+
+function submitComment() {
+    console.log('Hello!');
+};
