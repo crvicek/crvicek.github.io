c36b56c0a2b1ae00b1ac071d6406bc55a20d8f49
crvicek <crvicek@yahoo.com>
2019-01-09 11:14:52 +0100

    Create elements with data from form

--- detail-page.js
+++ detail-page.js
@@ -10,5 +10,29 @@ Empty the input fields so the next user can type some new text.
 */
 
 function submitComment() {
-    console.log('Hello!');
+    const inputField = document.getElementById('name');
+    const name = inputField.value;
+    
+    const textArea = document.getElementById('msg');
+    const msg = textArea.value;
+
+    const comment = document.createElement('section');
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p');
+
+    h3.innerHTML = `${name} said:`;
+    p.innerHTML = msg;
+
+    comment.classList.add('comment');
+    comment.appendChild(h3);
+    comment.appendChild(p);
+
+    //document.innerHTML.body..appendChild(comment);?
+
+    
+    console.log(comment);
+    //console.log(msg);
+        
+
+
 };
